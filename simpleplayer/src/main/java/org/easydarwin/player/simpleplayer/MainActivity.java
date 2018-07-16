0ed08ff47976319f262711ce4139171c3a6fd74d
gexiaopeng <gexiaopeng@tom.com>
2018-07-16 16:51:57 +0800

    Initial commit

---
+++ simpleplayer/src/main/java/org/easydarwin/player/simpleplayer/MainActivity.java
@@ -0,0 +1,30 @@
+package org.easydarwin.player.simpleplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.TextureView;
+
+import org.easydarwin.video.EasyPlayerClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextureView textureView = findViewById(R.id.texture_view);
+        /**
+         * 参数说明
+         * 第一个参数为Context,第二个参数为KEY
+         * 第三个参数为的textureView,用来显示视频画面
+         * 第四个参数为一个ResultReceiver,用来接收SDK层发上来的事件通知;
+         * 第五个参数为I420DataCallback,如果不为空,那底层会把YUV数据回调上来.
+         */
+        EasyPlayerClient client = new EasyPlayerClient(this, BuildConfig.KEY, textureView, null, null);
+        String url="rtsp://admin:12345@192.168.10.233:554/h264/ch1/main/av_stream";
+        url="rtsp://admin:adminINLJHY@192.168.10.234:554/h264/ch1/av_stream";
+        client.play(url);
+
+    }
+}
